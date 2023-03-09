8acd8e0435d8e43236eb8d838d460a1d4a609aeb
khang2038 <trankhang9c2003@gmail.com>
2023-03-10 00:23:41 +0700

    feat: change migration, add config service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
@@ -8,16 +8,31 @@ export class AuthService {
   constructor(private prisma: PrismaService) {}
   async signup(dto: AuthDto) {
     const hash = await argon.hash(dto.password);
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
-    return user;
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+      return user;
+    } catch (error) {
+      throw new ForbiddenException('Credentials taken');
+    }
   }
 
-  signin() {
-    return { msg: 'I have signed in' };
+  async signin(dto: AuthDto) {
+    const user = await this.prisma.user.findFirstOrThrow({
+      where: {
+        email: dto.email,
+      },
+    });
+    const match = await argon.verify(user.hash, dto.password);
+    if (!match) {
+      throw new ForbiddenException();
+    }
+    delete user.hash;
+    return user;
   }
 }
